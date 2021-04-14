094273a46c3a555b658eb40d9a30849873d17903
Theodore McElhatten <blackstone@bsstudios.net>
2021-04-14 17:43:00 +0000

    Test Piece

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -8,6 +8,10 @@ extern "C" void _start(BootInfo* bootInfo){
     KernelInfo kernelInfo = InitializeKernel(bootInfo);
     PageTableManager* pageTableManager = kernelInfo.pageTableManager;
 
+    canvas.CursorPosition = {0, 0};
+    canvas.Print("BsOs  Version Alpha 10");
+    canvas.CursorPosition = {0, 32};
+
     canvas.CursorPosition = {0, canvas.CursorPosition.Y + 16};
     canvas.Print("Free RAM: ");
     canvas.Print(to_string(GlobalAllocator.GetFreeRAM() / 1024));
@@ -24,11 +28,11 @@ extern "C" void _start(BootInfo* bootInfo){
     canvas.Print(" KB ");
     canvas.CursorPosition = {0, canvas.CursorPosition.Y + 16};
 
-    // for (int t = 0; t < 20; t++){
-    //     void* address = GlobalAllocator.RequestPage();
-    //     canvas.Print(to_hstring((uint64_t)address));
-    //     canvas.CursorPosition = {0, canvas.CursorPosition.Y + 16};
-    // }
+    for (int t = 0; t < 20; t++){
+        void* address = GlobalAllocator.RequestPage();
+        canvas.Print(to_hstring((uint64_t)address));
+        canvas.CursorPosition = {0, canvas.CursorPosition.Y + 16};
+    }
 
     while(true);
 
