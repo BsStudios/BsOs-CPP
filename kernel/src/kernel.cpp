2d4527f635f9d6b4a61b93a78af2a39bf2e99a58
Theodore McElhatten <blackstone@bsstudios.net>
2021-04-16 16:48:33 +0000

    Fix For Render

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -9,30 +9,30 @@ extern "C" void _start(BootInfo* bootInfo){
     KernelInfo kernelInfo = InitializeKernel(bootInfo);
     PageTableManager* pageTableManager = kernelInfo.pageTableManager;
 
-    globalConsole.CursorPosition = {0, 0};
-    globalConsole.Print("BsOs  Version Alpha 10");
-    globalConsole.CursorPosition = {0, 32};
+    globalConsole->CursorPosition = {0, 0};
+    globalConsole->Print("BsOs  Version Alpha 10");
+    globalConsole->CursorPosition = {0, 32};
 
-    globalConsole.CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
-    globalConsole.Print("Free RAM: ");
-    globalConsole.Print(to_string(GlobalAllocator.GetFreeRAM() / 1048576));
-    globalConsole.Print(" MB ");
+    globalConsole->CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
+    globalConsole->Print("Free RAM: ");
+    globalConsole->Print(to_string(GlobalAllocator.GetFreeRAM() / 1048576));
+    globalConsole->Print(" MB ");
+    globalConsole->CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
+
+    globalConsole->Print("Used RAM: ");
+    globalConsole->Print(to_string(GlobalAllocator.GetUsedRAM() / 1048576));
+    globalConsole->Print(" MB ");
     globalConsole.CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
 
-    globalConsole.Print("Used RAM: ");
-    globalConsole.Print(to_string(GlobalAllocator.GetUsedRAM() / 1048576));
-    globalConsole.Print(" MB ");
-    globalConsole.CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
-
-    globalConsole.Print("Reserved RAM: ");
-    globalConsole.Print(to_string(GlobalAllocator.GetReservedRAM() / 1048576));
-    globalConsole.Print(" MB ");
-    globalConsole.CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
+    globalConsole->Print("Reserved RAM: ");
+    globalConsole->Print(to_string(GlobalAllocator.GetReservedRAM() / 1048576));
+    globalConsole->Print(" MB ");
+    globalConsole->CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
 
     for (int t = 0; t < 20; t++){
         void* address = GlobalAllocator.RequestPage();
-        globalConsole.Print(to_hstring((uint64_t)address));
-        globalConsole.CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
+        globalConsole->Print(to_hstring((uint64_t)address));
+        globalConsole->CursorPosition = {0, globalConsole->CursorPosition->Y + 16};
     }
 
     while(true);
